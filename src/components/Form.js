b5ebbd663fcd472e43acba961ff88b24a99d8ae3
Joseph Noblett <noblettjoe@gmail.com>
2023-06-12 10:08:00 -0500

    built out form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,48 @@
 
-import { useState } from "react"
 import '../App.css';
 export default function Form(props) {
-    const [member, setMember] = useState()
+
+    const handleChange = evt => {
+        const { name, value } = evt.target;
+        props.change(name, value);
+    }
+
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        props.submit();
+    }
+
     return (
-        <form>
-            <h3>Add New Team Member</h3>
+        <form onSubmit={handleSubmit}>
+            <h3>Team Builder</h3>
             <label>
                 Name: <input 
+                placeholder="Ex. John Doe"
+                name="name"
                 type='text' 
+                value={props.values.name}
+                onChange={handleChange}
                 />
             </label>
             <label>
                 Email: <input 
+                placeholder="Ex. xxxxx@email.com"
+                name="email"
                 type='email' 
+                value={props.values.email}
+                onChange={handleChange}
                 />
             </label>
             <label>
                 Role: <input 
+                placeholder="Ex. Software Developer"
+                name="role"
                 type='text' 
+                value={props.values.role}
+                onChange={handleChange}
                 />
             </label>
-            <button>Submit</button>
+            <input type="submit" value="Create your team!" style={{width: '20%', margin: '0 auto'}} />
         </form>
     )
 }
