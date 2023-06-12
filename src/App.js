af6224110def133886ff8614a05a22096a57e237
Joseph Noblett <noblettjoe@gmail.com>
2023-06-12 10:07:30 -0500

    added form and mapped overr members

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 
 function App() {
-  const [team, setTeam] = useState()
+  const [members, setMembers] = useState([]);
+  const [values, setValues] = useState({name: '', email: '', role: ''});
+
+  const onSubmit = () => {
+    setMembers([values, ...members]);
+    setValues({name: '', email: '', role: ''})
+  }
+
+  const onChange = (name, value) => {
+    setValues({ ...values, [name]: value})
+  }
 
   return (
     <div className="app">
-      <Form />
+      <Form 
+        values={values}
+        change={onChange}
+        submit={onSubmit}
+      />
+      {members.map((member, idx) => {
+        return (
+          <div id={idx} className='list'>
+            Name: {member.name}, Email: {member.email}, Role: {member.role}, <button>Edit</button>
+            
+          </div>
+        )
+      })}
     </div>
   );
 }
